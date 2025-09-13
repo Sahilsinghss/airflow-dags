9977ec7da609cc1f4efd884be599ac535050e291
Sahilsinghss <87169358+Sahilsinghss@users.noreply.github.com>
2025-09-13 22:22:19 +0800

    Update hello-world-dag.py

--- dags/hello-world-dag.py
+++ dags/hello-world-dag.py
@@ -6,7 +6,7 @@ from datetime import datetime
 with DAG(
     dag_id="hello_world_dag",
     start_date=datetime(2024, 1, 1),
-    schedule_interval=None,  # Trigger manually
+    schedule=None,  # Trigger manually
     catchup=False,
     tags=["example"],
 ) as dag:
